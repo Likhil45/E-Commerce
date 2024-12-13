f2b499d1f03b98708cb7ac2dbf3fb9a6aa154bbf
Likhil45 <likhilcharan.vellanki99@gmail.com>
2024-12-13 13:16:32 -0600

    Get by id is done

--- Controller/server.go
+++ Controller/server.go
@@ -22,6 +22,18 @@ func TestHandler(w http.ResponseWriter, r *http.Request) {
 func GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(products)
 }
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	for _, item := range products {
+		if item.ProductId == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&model.Product{})
+}
+
 func StartServer() {
 	r := mux.NewRouter()
 
@@ -32,6 +44,7 @@ func StartServer() {
 	r.HandleFunc("/", HomeHandler).Methods("GET")
 	r.HandleFunc("/test", TestHandler)
 	r.HandleFunc("/all", GetAllProducts)
+	r.HandleFunc("/{id}", GetProductById)
 
 	http.ListenAndServe(":8080", r)
 
