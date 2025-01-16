9b60001a9bf5d6d06a1c414ca54c4f35e934f7bd
Likhil45 <likhilcharan.vellanki99@gmail.com>
2025-01-16 13:13:08 -0700

    Create user

--- Controller/userController.go
+++ Controller/userController.go
@@ -3,14 +3,17 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/Likhil45/E-Commerce/model"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/Likhil45/E-Commerce/database"
 	"github.com/Likhil45/E-Commerce/responses"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -42,6 +45,44 @@ func GetAllUser(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	response := responses.ProductResponse{Status: http.StatusOK, Message: "Successfully Fetched all Users", Data: map[string]interface{}{"users": users}}
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Create User
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var user model.User
+	var validate = validator.New()
+	defer cancel()
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println("Wrong data is sent")
+		panic(err)
+	}
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := responses.ProductResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": validationErr.Error()}}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	newUser := model.User{
+		UserName:     user.UserName,
+		UserId:       primitive.NewObjectID(),
+		UserPassword: user.UserPassword,
+		UserEmail:    user.UserEmail,
+		UserAddress:  user.UserAddress,
+	}
+	result, err := userCollection.InsertOne(ctx, newUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := responses.ProductResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	response := responses.ProductResponse{Status: http.StatusCreated, Message: "Succefully Added", Data: map[string]interface{}{"Added": result}}
 	json.NewEncoder(w).Encode(response)
 
 }
